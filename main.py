7435e30d4a74a93fb895c7dafc6af56e150a63b0
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-10-29 16:08:54 -0400

    commit 3

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import turtle               #1. import modules
 import random
-import math
 
 #Part A
-window = turtle.Screen()       # 2.  Create a screen
+window = turtle.Screen() 
+window.title("Turtle Race")      # 2.  Create a screen
 window.bgcolor('lightblue')
 
 michelangelo = turtle.Turtle()    # 3.  Create two turtles
@@ -24,12 +24,46 @@ def race(michelangelo,leonardo):
 		michelangelo.forward(random.randrange(0,10))
 		leonardo.forward(random.randrange(0, 10))
 
+def winner(michelangelo,leonardo):
+	return michelangelo.pos() > leonardo.pos()
+
+def positonTurtle(michelangelo,leonardo):
+	michelangelo.pos()
+	leonardo.pos()
+	print("michelangelo position: ",michelangelo.pos())
+	print("leonardo.position: ",leonardo.pos()) 
+	
+def distance(michelangelo,leonardo):
+	return michelangelo.pos()-leonardo.pos()
 		
 # Part B - complete part B here
-
 def main():
+	guess = ""
+	guesses = False
+	while guesses is False:
+		guess = str(input("guess the winner: "))
+		if (guess == "michelangelo" or guess == "leonardo"):
+		 	guesses = True
 	michelangelo.down()
 	race(michelangelo,leonardo)
+	if winner(michelangelo,leonardo):
+		print( "MICHELANGELO WINS!")
+		leonardo.setpos(-100,200)
+		leonardo.write("Winner: Michelangelo",font = ["Arial", 30, "normal"])
+		if guess == "michelangelo":
+			print("your guess is correct!")
+		else:
+			print("your guess is wrong!")
+	else:
+		print( "LEONARDO WINS!")
+		leonardo.setpos(-100,200)
+		leonardo.write("Winner: Leonardo",font = ["Arial", 30, "normal"])
+		if guess == "leonardo":
+			print("your guess is correct!")
+		else:
+			print("your guess is wrong!")
+	positonTurtle(michelangelo,leonardo)
+	print ("The distance between the winner and the loser is:",  distance(michelangelo,leonardo))
 	for shapeSides in [3,4,6,9,12]:
 		michelangelo.clear()
 		for k in range(shapeSides):
